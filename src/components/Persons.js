a2da7becc1213b5c14051b582b40aac4860f01b6
= <rumbawajomar@gmail.com>
2022-04-27 17:02:22 -0700

    create Persons component

---
+++ src/components/Persons.js
@@ -0,0 +1,13 @@
+const Persons = ({ filteredNames }) => {
+  return (
+    <>
+      {filteredNames.map((person) => (
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
+      ))}
+    </>
+  )
+}
+
+export default Persons
