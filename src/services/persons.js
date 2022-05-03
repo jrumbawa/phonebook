91bbf6f9d896af9e909815fd5bdd9b2dc31f049f
= <rumbawajomar@gmail.com>
2022-05-03 12:12:16 -0700

    move person services out of components into src

--- src/services/persons.js
+++ src/services/persons.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
-  const request = axios.get('http://localhost:3001/persons')
+  const request = axios.get(baseUrl)
   return request.then((response) => response.data)
 }
 
