3dfab5c61ee896a466b2cd7adfeba2a0b67ba2b5
jrumbawa <rumbawajomar@gmail.com>
2023-12-09 16:52:10 -0800

    update PeopleList.jsx

--- src/components/PeopleList.jsx
+++ src/components/PeopleList.jsx
@@ -1,10 +1,18 @@
-const PeopleList = ({ persons, filter }) => {
+import Person from './Person'
+
+const PeopleList = ({ persons, filter, deletePerson }) => {
   const filteredPeople = persons.filter(person => person.name.includes(filter))
 
   return (
     <ul>
-    {filteredPeople.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
-  </ul>
+      {filteredPeople.map(person => 
+        <Person 
+          person={person} 
+          key={person.id} 
+          deletePerson={() => deletePerson(person.id, person.name)}
+        />
+      )}
+    </ul>
   )
 }
 
