5797fea79ae31d20e209be8736b9ebd6a98f258f
jrumbawa <rumbawajomar@gmail.com>
2023-12-09 16:51:17 -0800

    Create Person.jsx

---
+++ src/components/Person.jsx
@@ -0,0 +1,12 @@
+const Person = ({ person, deletePerson }) => {
+  return (
+    <>
+      <li key={person.name}>
+        {person.name} {person.number}
+        <button onClick={deletePerson}>delete</button>
+      </li>
+    </>
+  )
+}
+
+export default Person
